beca924b99e4422d445a4bb1bff77d25858acd42
Manuel Sebastián Torres Hernandez <manuel.torres@FZCORP.local>
2026-02-27 14:01:38 -0500

    Arreglos finales

--- poc1/entrypoint.sh
+++ poc1/entrypoint.sh
@@ -5,7 +5,7 @@ echo "--- Iniciando verificación de modelo ---"
 echo "Tracking URI: $MLFLOW_TRACKING_URI"
 
 URL="${MLFLOW_TRACKING_URI}/api/2.0/mlflow/registered-models/alias?name=RF_test_1&alias=production"
-curl -v "${MLFLOW_TRACKING_URI}/api/2.0/mlflow/registered-models/alias?name=RF_test_1&alias=production"
+# curl -v "${MLFLOW_TRACKING_URI}/api/2.0/mlflow/registered-models/alias?name=RF_test_1&alias=production"
 
 until curl -s -f "$URL" > /dev/null 2>&1; do
   echo "Modelo RF_test_1@production no encontrado aún. Reintentando en 10s..."
